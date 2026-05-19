4b603bd475e3df31aeb54b25af78b69a54a6075b
Oxy083612 <micha.durka7@gmail.com>
2026-05-19 18:51:07 +0200

    added hiding spot

--- assets/hiding_spot.gd
+++ assets/hiding_spot.gd
@@ -1,26 +1,24 @@
-class_name Room
-
 extends Node2D
 
-var y_movement := 450;
-var input_flag := false
+var input_flag = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-	
+	pass # Replace with function body.
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
 
-func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if input_flag:
 		return
 
 	if event is InputEventMouseButton and event.pressed:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			input_flag = true
-			SignalBus.movement_action.emit(get_global_mouse_position().x, y_movement)
+			SignalBus.hiding_action.emit(position.x, 450,  position.y)
 	
 	input_flag = false
-	
