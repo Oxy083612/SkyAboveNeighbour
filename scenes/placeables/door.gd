29af05015a01c60ebf497d35dcedb3d957388bd1
Oxy083612 <micha.durka7@gmail.com>
2026-05-20 20:23:33 +0200

    medals working

--- scenes/placeables/door.gd
+++ scenes/placeables/door.gd
@@ -7,6 +7,8 @@ extends Area2D
 @export var destination_door : Door
 @onready var destination_point = $DestinationPoint
 
+var input_flag = false
+
 func _get_exit_position() -> Vector2:
 	return destination_point.global_position
 
@@ -15,3 +17,16 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.is_class("CharacterBody2D"):
 		var enemy = body as Enemy
 		enemy.change_floor(destination_floor, $DestinationPoint.global_position, self)
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if input_flag:
+		return
+
+	if event is InputEventMouseButton and event.pressed:
+		print("UAUAU")
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			input_flag = true
+			SignalBus.door_action.emit(position.x, 450 * (destination_floor + 1), destination_point)
+	
+	input_flag = false
