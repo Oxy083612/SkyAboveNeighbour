aeaf3860836baaef78c4544e64e0818ce4b0219b
Oxy083612 <micha.durka7@gmail.com>
2026-05-20 15:03:12 +0200

    debug level change

--- levels/debug_level.gd
+++ levels/debug_level.gd
@@ -5,9 +5,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for x in get_tree().get_first_node_in_group("Pranks")
-		var prank = x as Prank
-		player.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
