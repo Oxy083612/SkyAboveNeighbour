2625706fedc037df590f14171775ad2cadc079ed
Oxy083612 <micha.durka7@gmail.com>
2026-05-20 14:44:55 +0200

    medals

--- UI/medals_logic.gd
+++ UI/medals_logic.gd
@@ -12,11 +12,11 @@ var medal_none = preload("res://UI/pngs/faceon_silver_coin.png")
 var medal_good = preload("res://UI/pngs/gold_trophy.png")
 var medal_bad = preload("res://UI/pngs/face_on_cross.png")
 
-@onready var texture = $CenterContainer/TextureRect
+@onready var texture = $TextureRect
 
-func _init() -> void:
+func _ready() -> void:
 	current_state = MedalState.NONE
-	texture.texture = medal_bad
+	texture.texture = medal_none
 	
 func _change_medal(name):
 	match name:
--- levels/debug_level.gd
+++ levels/debug_level.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+
+@onready var player = $PlayerCharacter
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	for x in get_tree().get_first_node_in_group("Pranks")
+		var prank = x as Prank
+		player.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
