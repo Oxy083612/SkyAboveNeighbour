ce3bc52d088d442fcf708f2a273c2c6b89ea3d66
Anna <nikola.zabawa@onet.pl>
2026-05-20 12:23:46 +0200

    Add stuff or sum

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -5,10 +5,15 @@ func addItem(name):
 	pickedUpItems.append(name)
 	print(name)
 func checkItem(name):
-	return pickedUpItems.has(name)
+	print(name)
+	for index in range(pickedUpItems.size()):
+		print(pickedUpItems[index].itemName+"a")
+		if pickedUpItems[index].itemName == name:
+			return true
+	return false
 func getIndex(name):
 	for index in range(pickedUpItems.size()):
-		if pickedUpItems[index] == name:
+		if pickedUpItems[index].itemName == name:
 			return index
 	return -1
 func removeItem(name):
