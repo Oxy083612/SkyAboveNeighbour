7201e6535279b68e90322aba202f6cdbf9d66929
Shimaru0 <fedyniaktadeusz1@gmail.com>
2026-05-20 14:49:40 +0200

    ai2

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,6 +1,6 @@
-extends Area2D
-class_name Door
 
+class_name Door
+extends Area2D
 @export var current_floor : int
 @export var destination_floor : int
 
@@ -11,4 +11,7 @@ func _get_exit_position() -> Vector2:
 	return destination_point.global_position
 
 func _on_body_entered(body: Node2D) -> void:
-	body.set_position($DestinationPoint.global_position)
+	#body.set_position($DestinationPoint.global_position)
+	if body.is_class("CharacterBody2D"):
+		var enemy = body as Enemy
+		enemy.change_floor(destination_floor, $DestinationPoint.global_position, self)
